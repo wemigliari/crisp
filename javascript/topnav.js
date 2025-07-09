000f24c1355df50e41eac48d3b6e41349df4bb33
wemigliari <wemigliari@yahoo.com.br>
2025-07-09 13:49:49 +0200

    First commit

---
+++ javascript/topnav.js
@@ -0,0 +1,21 @@
+/**
+ * @Author: wellingtonmigliari
+ * @Date:   2022-10-11T17:26:33+02:00
+ * @Last modified by:   wellingtonmigliari
+ * @Last modified time: 2023-03-22T17:45:46+01:00
+ */
+
+topnav.innerHTML = `
+<div class="topnav">
+	<div class="topnav-1">
+	<a style="color:#56675c;" href="index.html"><img src="images/crisp_logo.png" style="height: 35%; width: 55%;"></a>
+	</div>
+	<ol class="fh5co-social-icons" style="padding-top: 10px;">
+		<a style="margin-left: -2px;" href="https://www.crisp.ufmg.br:8080" target="_blank"><i class="fa fa-solid fa-user w3-xlarge" style="font-size: 29px; margin-right: 10px;"></i></a>
+		<a style="margin-left: 5px;" href="https://www.instagram.com/crisp.ufmg/?hl=en" target="_blank"><i class="fa fa-instagram w3-xlarge" style="margin-right: 10px;"></i></a>
+		<a style="margin-left: 5px;" href="https://www.facebook.com/CrispUFMG/" target="_blank"><i class="fa fa-facebook w3-xlarge"></i></a>
+		<a style="margin-left: 5px;" href="https://open.spotify.com/show/08kfV9vcmBXKh8CQ3n8zaH" target="_blank"><i class="fa fa-spotify w3-xlarge"></i></a>
+		<a style="margin-left: 5px;" href="search.html" target="_blank"><i class="fa fa-search w3-xlarge"></i></a>
+	</ol>
+</div>
+`;
