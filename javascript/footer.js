a371db39755e5dc2fb7fdef6becb848d2662f858
wemigliari <wemigliari@yahoo.com.br>
2025-11-09 13:24:16 +0100

    First commit

---
+++ javascript/footer.js
@@ -0,0 +1,76 @@
+/**
+ * @Author: wellingtonmigliari
+ * @Date:   2022-10-11T17:26:33+02:00
+ * @Last modified by:   wellingtonmigliari
+ * @Last modified time: 2025-11-09T00:47:13+01:00
+ */
+
+footer.innerHTML = `
+<footer id="fh5co-footer" role="contentinfo" style="padding-bottom: 60px;">
+  <div class="container" style="background-color: #c4ccc4;">
+    <div class="row row-pb-md">
+      <div class="col-md-6 fh5co-widget" style="margin-top: -5px;">
+        <h3 style="color:#fff; font-size: 40px; margin-bottom: 10px;">crisp</h3>
+        <div style="color:#fff;">
+        <p>
+        Av. Presidente Antônio Carlos, nº 6627 <br>Pampulha - Unidade Administrativa III <br>
+        Universidade Federal de Minas Gerais <br>
+        Tel.: (31) 3409-6310/3409-6306 <br>
+        E-mail: crisp@crisp.ufmg.br
+        </p></div>
+      </div>
+      <div class="col-md-3" style="padding-top: 50px;">
+        <ol class="fh5co-footer-links">
+          <li><a href="sobre_crisp.html">sobre</a></li>
+          <li><a href="equipe.html">equipe</a></li>
+          <li><a href="cade_meus_direitos.html">cadê meus direitos?</a></li>
+          <li><a href="erasmus.html">erasmus-success</a></li>
+          <li><a href="publicacoes_crisp_artigos.html">artigos</a></li>
+        </ol>
+      </div>
+      <div class="col-md-3" style="padding-top: 50px;">
+        <ol class="fh5co-footer-links">
+          <li><a href="entrevistas.html">crisp podcasts</a></li>
+          <li><a href="politica_protecao_dados.html">privacidade</a></li>
+          <li><a href="politica_protecao_dados.html">proteção de dados</a></li>
+          <li><a href="midia_noticias.html">imprensa</a></li>
+            <li><a href="covid.html">covid-19</a></li>
+        </ol>
+      </div>
+    </div>
+
+    <div class="row copyright">
+      <div class="col-md-12 text-center">
+        <p>
+          <small class="block" style="color: #5a7c6b;">&copy; 2022. All Rights Reserved.</small>
+          <small class="block" style="color: #5a7c6b">Criado por <a href="http://lattes.cnpq.br/2780207265936546" style="color: #5a7c6b;" target="_blank">W. Migliari</a></small>
+          <small class="block" style="color: #5a7c6b">Webpage financiada pelo Projeto Cadê meus direitos?</small>
+        </p>
+        <p>
+          <ol class="fh5co-social-icons" style="padding-top: 10px;">
+            <a style="margin-left: 15px;" href="https://www.instagram.com/crisp.ufmg/?hl=en" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
+            <a style="margin-left: 15px;" href="https://www.linkedin.com/company/crisp-ufmg/?originalSubdomain=br" target="_blank"><i class="fa fa-linkedin fa-2x"></i></a>
+            <a style="margin-left: 15px;" href="https://open.spotify.com/show/08kfV9vcmBXKh8CQ3n8zaH" target="_blank"><i class="fa fa-spotify fa-2x"></i></a>
+            <!--
+            <a style="margin-left: 15px;" href="#"><i class="fa fa-dribbble fa-2x"></i></a>
+            -->
+          </ol>
+        </p>
+      </div>
+      <div class="col-md-12 text-center">
+        <p>
+          <ol class="fh5co-social-icons" style="padding-top: 10px;">
+            <a style="margin-left: 15px;" href="http://www.fafich.ufmg.br/ppgs/linhas-de-pesquisa/" target="_blank"><img src="images/ppgs_ufmg.png" alt="user" style="height: 10%; width: 20%;"></a>
+            <a style="margin-left: 15px;" href="https://ufmg.br/" target="_blank"><img src="images/UFMG-Logo.png" alt="user" style="height: 10%; width: 10%;"></a>
+            <!--
+            <a style="margin-left: 15px;" href="https://vimeo.com/showcase/9737397?page=2" target="_blank"><img src="images/cade_meus_direitos2.png" alt="user" style="padding-left: 0px; margin-top: -10px; width: 25%;"></a>
+            <a style="margin-left: 15px;" href="http://criminologia.successproject.eu/" target="_blank"><img src="images/success.png" alt="user" style="height: 16%; width: 16%; margin-top: -5px;"></a>
+            -->
+          </ol>
+        </p>
+      </div>
+    </div>
+
+  </div>
+</footer>
+`;
---
+++ javascript/header.js
@@ -0,0 +1,124 @@
+/**
+ * @Author: wellingtonmigliari
+ * @Date:   2022-10-11T17:26:33+02:00
+ * @Last modified by:   wellingtonmigliari
+ * @Last modified time: 2025-11-09T11:54:26+01:00
+ */
+
+header.innerHTML = `
+<div class="area">
+</div>
+<nav class="main-menu">
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<a href="index.html" style="height: 68px;"><i class="fa fa-home fa-2x" style="font-size: 2.9em; margin-left: 12px; margin-right: 12px; margin-top: 20px;"></i></a>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2" style="margin-top: 5px; margin-bottom: 5px;">
+		<button class="dropbtn-2"><i class="fa fa-duotone fa-people-group fa-2x" style="font-size: 2.1em; margin-left: -4px;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="sobre_crisp.html" style="height: 68px;">sobre</a>
+				<a href="memoria_crisp.html" style="height: 68px;">memória</a>
+				<a href="equipe.html" style="height: 68px;">equipe</a>
+			</div>
+	</div>
+</li>
+
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-graduation-cap fa-2x"></i></button>
+			<div class="dropdown-content-2">
+				<a href="pos_graduacao.html" style="height: 68px;">pós-graduação</a>
+				<a href="especializacao.html" style="height: 68px;">especialização</a>
+			</div>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-book-open-reader fa-2x"></i></button>
+			<div class="dropdown-content-2" style="min-width: 224px;">
+				<a href="cade_meus_direitos.html" style="height: 68px;">cadê meus direitos?</a>
+				<a href="erasmus.html" style="height: 68px;">erasmus-success</a>
+				<a href="politicas_publicas.html" style="height: 68px;">políticas públicas CRISP</a>
+				<a href="projetos.html" style="height: 68px;">todos os projetos</a>
+			</div>
+	</div>
+</li>
+
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-book fa-2x"></i></button>
+			<div class="dropdown-content-2">
+				<a href="publicacoes_crisp_artigos.html" style="height: 68px;">artigos</a>
+				<a href="publicacoes_crisp_livros_teses_dissertacoes.html" style="height: 68px;">livros</a>
+				<a href="publicacoes_crisp_livros_teses_dissertacoes.html#fh5co-teses-dissertacoes" style="height: 68px;">teses & dissertações</a>
+			</div>
+	</div>
+</li>
+<li>
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-timeline fa-2x"></i></button>
+		<div class="dropdown-content-2" style="min-width: 224px;">
+		<a href="linha_do_tempo_unidades_prisionais.html" style="height: 68px;">sobre unidades prisionais</a>
+		<a href="linha_do_tempo.html" style="height: 88px;">sobre legislação para as prisões</a>
+		<a href="linha_do_tempo_fatos_marcantes.html" style="height: 88px;">fatos marcantes para o sistema prisional mineiro</a>
+		</div>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-regular fa-podcast fa-2x"></i></button>
+			<div class="dropdown-content-2">
+			<a href="midia_noticias.html" style="height: 68px;">imprensa</a>
+			<a href="entrevistas.html" style="height: 68px;">podcasts</a>
+			</div>
+	</div>
+</li>
+<li>
+	<div class="dropdown-2" style="margin-top: 0px; margin-bottom: 0px;">
+		<button class="dropbtn-2"><i class="fa-regular fa-pen-to-square fa-2x"></i></button>
+			<div class="dropdown-content-2">
+				<a href="por_elas.html" style="height: 66px;">por elas</a>
+			</div>
+	</div>
+</li>
+
+<li>
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa-regular fa-newspaper fa-2x"></i></button>
+			<div class="dropdown-content-2">
+				<a href="newsletter.html" style="height: 66px;">newsletter</a>
+			</div>
+	</div>
+</li>
+
+<li>
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-database fa-2x"></i></button>
+			<div class="dropdown-content-2">
+				<a href="dados.html" style="height: 68px;">repositório de dados</a>
+				<a href="repositorio.html" style="height: 85px;">projeto repositório de dados</a>
+			</div>
+	</div>
+</li>
+<li>
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa-solid fa-magnifying-glass fa-2x"></i></button>
+			<div class="dropdown-content-2">
+				<a href="search.html" style="height: 66px;">arquivo</a>
+			</div>
+	</div>
+</li>
+<!--
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-search fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="search.html" style="height: 68px;">pesquisar</a>
+			</div>
+	</div>
+</li>
+-->
+</nav>
+`;
